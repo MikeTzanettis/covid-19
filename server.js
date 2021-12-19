f1a3e5a50d3ad462047de36d45ddb927d6daad49
miketz <mtzanettis@gmail.com>
2021-12-19 19:58:20 +0200

    Third commit

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("Hey there, How are you?")
+const express = require('express')
+
+app = express()
+
+app.set('view-engine','ejs')
+app.get('/', (req,res)=>{
+    res.render('index.ejs')
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen("3000")
